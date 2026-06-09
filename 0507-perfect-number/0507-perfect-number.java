8a67df833ea8c3d92867d94ba38da027cb5eabe9
DhanashriShegokar7 <dhanashrishegokar7@gmail.com>
2026-06-09 12:10:12 +0530

    Time: 2 ms (80.28%), Space: 42.2 MB (25.19%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
+        if (num <= 1) return false;
 
-        int sum = 0;
+        int sum = 1; // 1 is always a divisor
 
-        for (int i = 1; i < num; i++) {
-
+        for (int i = 2; i * i <= num; i++) {
             if (num % i == 0) {
                 sum += i;
+
+                if (i != num / i) {
+                    sum += num / i;
+                }
             }
         }
-        if(sum == num) {
-            return true;
-        } else {
-            return false;
-        }
 
+        return sum == num;
     }
 }
