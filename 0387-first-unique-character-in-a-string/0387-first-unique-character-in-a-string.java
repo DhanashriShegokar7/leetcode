83485c8803d67ea686a72c6e565b957be42ca424
Dhanashri Mahendra Shegokar <dhanashrishegokar7@gmail.com>
2026-07-19 15:06:43 +0530

    Time: 21 ms (50.21%), Space: 49 MB (6.43%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -3,8 +3,8 @@ class Solution {
         int[] freq = new int[26];
         Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            freq[ch - 'a']++;
+            //char ch = s.charAt(i);
+            freq[s.charAt(i) - 'a']++;
             q.add(i);   // index store करतो
             while (!q.isEmpty() && freq[s.charAt(q.peek()) - 'a'] > 1) {
                 q.remove();
